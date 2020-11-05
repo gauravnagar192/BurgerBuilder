48e577ef7d31b51b552f4e4bbaff4b3cd7357dff
gauravnagar11 <gauravnagar192@gmail.com>
2020-11-05 10:50:49 +0530

    navigation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerLogo from '../../assests/images/burger-logo.png';
+import classes from './Logo.css';
+
+const Logo = (props) => (
+  <div className={classes.Logo}>
+    <img src={BurgerLogo} alt="MyBurger" />
+  </div>
+);
+
+export default Logo;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Toolbar.css';
 
 const Toolbar = () => (
   <header className={classes.Toolbar}>
     <div>MENU</div>
-    <div>LOGO</div>
-    <nav>...</nav>
+    <Logo />
+    <nav>
+      <NavigationItems />
+    </nav>
   </header>
 );
 export default Toolbar;
